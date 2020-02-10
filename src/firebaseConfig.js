bf33abf18226a0c15b60fe28f4062659374a65fd
AMAN SHARMA <amansharmaammu2@gmail.com>
2020-02-10 16:32:55 +0530

    first commit

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyBe4e1XCMEEyZt6Fn4CzVubOvfSJXUg5w0",
+    authDomain: "react-firebase-authentic-8c15d.firebaseapp.com",
+    databaseURL: "https://react-firebase-authentic-8c15d.firebaseio.com",
+    projectId: "react-firebase-authentic-8c15d",
+    storageBucket: "react-firebase-authentic-8c15d.appspot.com",
+    messagingSenderId: "368973633571",
+    appId: "1:368973633571:web:7678d806cb97ee575af996"
+  };
+  // Initialize Firebase
